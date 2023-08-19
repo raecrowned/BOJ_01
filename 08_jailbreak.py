a040c57c20326c284893b411776720c2b551505e
2019042906 <20190426906@hanyang.ac.kr>
2023-08-19 23:15:44 +0900

    8/19/2023

--- 08_jailbreak.py
+++ 08_jailbreak.py
@@ -1,64 +1,103 @@
 from collections import deque
 import sys
 input = sys.stdin.readline
-# Wall = '*', space = '.', door = '#', prisoner = '$'
 
-dyx = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+# Note: Top, Bottom, Right, Left boundaries are added to the map
+# That means +2 for h and w values!
 
-def openRegion(h, w, graph):
-    visited = [[0 for _ in range(w)] for _ in range(h)]
+dyx = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+def findPrisoner(h, w, jail):
+    plist = []
+    for y in range(h+2):
+        for x in range(w+2):
+            if jail[y][x] == '$':
+                plist.append([y, x])
+    return plist
+
+# bfs-travels through the grid, and creates a "route" of path cost
+# that increases for each door encountered
+def route(h, w, start, jail):
+    visited = [[-1]*(w+2) for _ in range(h+2)]
+    visited[start[0]][start[1]] = 0
     q = deque()
+    q.append(start)
 
-    for y in range(h):
-        for x in [0, w-1]:
-            if map[y][x] != '*':
-                q.append(y, x)
-    for y in [1, h-1]:
-        for x in range(w):
-            if map[y][x] != '*':
-                q.append(y, x)
     while q:
         cy, cx = q.popleft()
-        if map[cy][cx] == '#':
-            visited[cy][cx] = 1 # boundary door!
-            continue            # can't move anywhere from door w/o opening it!
-
-        count += 1
         for dy, dx in dyx:
-            ny, nx = y+dy, x+dx
-            if 0 <= ny < h and 0 <= nx < w and map[ny][nx] != '*' and not visited[ny][nx]:
-                visited[ny][nx] == 1
-                q.append((ny, nx))
+            ny, nx = cy+dy, cx+dx
+            if 0 <= ny < h+2 and 0 <= nx < w+2 and visited[ny][nx] < 0:
+                if jail[ny][nx] == '*':
+                    continue
+
+                if jail[ny][nx] == '#':
+                    visited[ny][nx] = visited[cy][cx]+1
+                    q.append((ny, nx))
+                else:
+                    visited[ny][nx] = visited[cy][cx]
+                    q.appendleft((ny, nx))
+
     return visited
 
-def toGraph(h, w, map):
-    graph = [[] for _ in range(h*w)]
-    prisoners = []
-    for y in range(h):
-        for x in range(w):
-            if map[y][x] == '*':
-                continue
-            if map[y][x] == '$':
-                prisoners.append((y, x))
-            weight = 1 if map[y][x] == '#' else 0
-            for dy, dx in dyx:
-                ny, nx = y+dy, x+dx
-                if 0 <= ny < h and 0 <= nx < w and map[ny][nx] != '*':
-                    # flatten ny, nx
-                    graph[w*y + x].append((w*ny + nx, weight))
-    return graph
+def jailbreak(h, w, jail):
+    startpoints = findPrisoner(h, w, jail)
+    startpoints.append([0, 0])              # adding boundary location
+    routes = {}
 
-def jailbreak(h, w, map):
-    openR = openRegion(h, w, map)
-    graph = toGraph(map)
-    # implement
-    return
+    for startpoint in startpoints:
+        # r is a dictionary of startpoint: route!
+        r = route(h, w, startpoint, jail)
+        routes[tuple(startpoint)] = r
+
+    minDoors = float('inf')
+    for y in range(h+2):
+        for x in range(w+2):
+            # check if a cell is visit-able for all 3 bfs traversals
+            # redundant, but:
+            # need to check if a cell is visited by bfs anyways, so leave it
+            allVisited = all(r[y][x] != -1 for _, r in routes.items())
+            if allVisited:
+                # sum the cost of reaching that cell!
+                totalDoors = sum(r[y][x] for _, r in routes.items())
+                # if it is a door cell, it's value has been +1'd for all three bfs'.
+                # account for that; subtract 2.
+                if jail[y][x] == '#':
+                    totalDoors -= 2
+                # update minimal door cost
+                minDoors = min(minDoors, totalDoors)
+
+    return minDoors
+
+def printGrid(grid):
+    for i in range(len(grid)):
+        print(''.join(grid[i]))
+    print()
+
+def printRoute(route):
+    for i in range(h+2):
+        for j in range(w+2):
+            if route[i][j] < 0:
+                print('_', end="")
+            else:
+                print(str(route[i][j]), end="")
+        print()
+    print()
+
 
 if __name__ == "__main__":
-    testcase = int(input())
+    testcase = int(input().strip())
+    solutions = []
 
     for i in range(testcase):
         h, w = map(int, input().strip().split())
-        map = [[char for char in input().strip()] for _ in range(h)]
-        sol = jailbreak(h, w, map)
-        print(sol)
+        jail = [[char for char in '.'+input().strip()+'.'] for _ in range(h)]
+
+        # adding boundaries
+        jail[:0] = ['.' * (w+2)]
+        jail += ['.' * (w+2)]
+
+        solutions.append(jailbreak(h, w, jail))
+
+    for solution in solutions:
+        print(solution)
